80b5f04181e3034f022ba0c28716324f014c6d8b
Ramful Devesh <deveshramful@outlook.com>
2023-02-12 00:07:26 -0500

    Added facade

---
+++ src/structural/facade/app.java
@@ -0,0 +1,12 @@
+package structural.facade;
+
+public class app {
+    public static void main(String[] args){
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+
+}
