c41f5ee027e8288850c3244a173611ed6fff3efd
Ramful Devesh <deveshramful@outlook.com>
2023-02-11 23:10:54 -0500

    Added singleton

---
+++ src/structural/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package structural.singleton;
+
+public class Singleton {
+    private static Singleton instance = null;
+
+    private Singleton(){
+        // Perform instance initializations
+    };
+
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
---
+++ src/structural/singleton/app.java
@@ -0,0 +1,14 @@
+package structural.singleton;
+
+public class app {
+
+
+    public static void  main(String[] args){
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        if (instance1 == instance2){
+            System.out.println("Same instance");
+        }
+    }
+}
